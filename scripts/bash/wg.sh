df0420e290f4733e69048e56dc2a7c887fe78471
herukrayz <ad20h4y@yahoo.com>
2020-07-01 15:44:30 +0700

    Update wg.sh

--- scripts/bash/wg.sh
+++ scripts/bash/wg.sh
@@ -7,6 +7,7 @@ CLIENT_TPL_FILE=../data/client.conf.tpl
 SERVER_TPL_FILE=../data/server.conf.tpl
 SAVED_FILE=../data/tmp/saved.tmp
 AVAILABLE_IP_FILE=../data/tmp/available_ip.tmp
+WG_CONF_FILE="/etc/wireguard/$_INTERFACE.conf"
 
 dec2ip() {
     local delim=''
